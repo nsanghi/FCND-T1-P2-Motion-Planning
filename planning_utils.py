b14a3460f9e5f7239d45187929dccc26ca1d9800
Nimish Sanghi <nimish.sanghi@gmail.com>
2018-04-01 04:20:11 +0530

    revied path_planning

--- planning_utils.py
+++ planning_utils.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
+from bresenham import bresenham
+
 
 
 def create_grid(data, drone_altitude, safety_distance):
@@ -55,6 +57,10 @@ class Action(Enum):
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
+    NORTH_WEST = (-1, -1, np.sqrt(2))
+    NORTH_EAST = (-1, 1, np.sqrt(2))
+    SOUTH_WEST = (1, -1, np.sqrt(2))
+    SOUTH_EAST = (1, 1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -85,6 +91,16 @@ def valid_actions(grid, current_node):
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
 
+    if (x - 1 < 0 or y - 1 < 0) or grid[x - 1, y - 1] == 1:
+        valid_actions.remove(Action.NORTH_WEST)
+    if (x - 1 < 0 or y + 1 > m) or grid[x - 1, y + 1] == 1:
+        valid_actions.remove(Action.NORTH_EAST)
+    if (x + 1 > n or y - 1 < 0) or grid[x + 1, y - 1] == 1:
+        valid_actions.remove(Action.SOUTH_WEST)
+    if (x + 1 > n or y + 1 > m) or grid[x + 1, y + 1] == 1:
+        valid_actions.remove(Action.SOUTH_EAST)
+
+
     return valid_actions
 
 
@@ -98,16 +114,16 @@ def a_star(grid, h, start, goal):
 
     branch = {}
     found = False
-    
+
     while not queue.empty():
         item = queue.get()
         current_node = item[1]
         if current_node == start:
             current_cost = 0.0
-        else:              
+        else:
             current_cost = branch[current_node][0]
-            
-        if current_node == goal:        
+
+        if current_node == goal:
             print('Found a path.')
             found = True
             break
@@ -118,12 +134,12 @@ def a_star(grid, h, start, goal):
                 next_node = (current_node[0] + da[0], current_node[1] + da[1])
                 branch_cost = current_cost + action.cost
                 queue_cost = branch_cost + h(next_node, goal)
-                
-                if next_node not in visited:                
-                    visited.add(next_node)               
+
+                if next_node not in visited:
+                    visited.add(next_node)
                     branch[next_node] = (branch_cost, current_node, action)
                     queue.put((queue_cost, next_node))
-             
+
     if found:
         # retrace steps
         n = goal
@@ -136,7 +152,7 @@ def a_star(grid, h, start, goal):
     else:
         print('**********************')
         print('Failed to find a path!')
-        print('**********************') 
+        print('**********************')
     return path[::-1], path_cost
 
 
@@ -144,3 +160,60 @@ def a_star(grid, h, start, goal):
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
 
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+def collinearity_int(p1, p2, p3):
+    collinear = False
+    collinear = p1[0]*(p2[1] - p3[1])+p2[0]*(p3[1] - p1[1])+p3[0]*(p1[1] - p2[1]) == 0
+
+def prune_path(path):
+    pruned_path = [p for p in path]
+    i = 0
+    while i < len(pruned_path) - 2:
+        # If the 3 points are in a line remove the 2nd point.
+        # The 3rd point now becomes and 2nd point
+        # and the check is redone with a new third point
+        # on the next iteration.
+        if collinearity_int(pruned_path[i], pruned_path[i+1], pruned_path[i+2]):
+            # Something subtle here but we can mutate
+            # `pruned_path` freely because the length
+            # of the list is check on every iteration.
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+    return pruned_path
+
+def bresenham_feasible(grid, p1, p2):
+    # first get all the x and y coordinates of cells that are covered
+    # from p1 to p2 using bresenham algorithm
+    cells = np.transpose(list(bresenham(p1[0], p1[1], p2[0], p2[1])))
+    # now use 'index arrays' indexing of numpy to get a flat array
+    # consisting of only the bresenham cells from grid
+    # and if all these cells contain zero (i.e. np.sum == 0),
+    # then p1 to p2 can be connected without hitting any obstacle
+    return np.sum(grid[cells[0], cells[1]]) == 0
+
+def bresenham_prune(grid, path):
+    pruned_path = []
+
+    #append start point to pruned path
+    i = 0
+    pruned_path.append(path[i])
+    # index i is the start point p1 for `bresenham_feasible`
+    while i < len(path) - 1:
+        p1 = path[i]
+        # iterate from j=i+1 to end till bresenham_feasible Returns
+        # false indicating that i and j-1 can be joined but not i and j
+        for j in range(i+1, len(path)):
+            p2 = path[j]
+            if not bresenham_feasible(grid, p1, p2):
+                # append j-1 as the next waypoint
+                # and then start all over from i=j-1 to end
+                pruned_path.append(path[j-1])
+                i = j-1
+                break
+        i += 1
+    # append the last waypoint from input path to pruned_path
+    pruned_path.append(path[-1])
+    return pruned_path
